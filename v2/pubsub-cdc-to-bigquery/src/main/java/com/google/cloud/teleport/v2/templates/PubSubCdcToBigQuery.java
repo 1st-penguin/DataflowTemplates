4f2dbff1782b027a5fd0c7d21b31cd97afd83432
yuemori <moonoverwalker@gmail.com>
2021-07-09 18:57:00 +0900

    Modify InsertRetryPolicy only transient errors cause

--- v2/pubsub-cdc-to-bigquery/src/main/java/com/google/cloud/teleport/v2/templates/PubSubCdcToBigQuery.java
+++ v2/pubsub-cdc-to-bigquery/src/main/java/com/google/cloud/teleport/v2/templates/PubSubCdcToBigQuery.java
@@ -377,7 +377,7 @@ public class PubSubCdcToBigQuery {
                 .withWriteDisposition(WriteDisposition.WRITE_APPEND)
                 .withExtendedErrorInfo()
                 .withMethod(BigQueryIO.Write.Method.STREAMING_INSERTS)
-                .withFailedInsertRetryPolicy(InsertRetryPolicy.alwaysRetry()));
+                .withFailedInsertRetryPolicy(InsertRetryPolicy.retryTransientErrors()));
 
     /*
      * Step 3 Contd.
